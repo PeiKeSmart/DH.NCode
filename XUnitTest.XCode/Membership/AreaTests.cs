f0c127834c6179ac239dca56010059c83a09573d
猿人易 <qcjxberin@outlook.com>
2025-02-27 12:17:00 +0800

    MySql驱动支持NewLife.MySql；整理代码

--- XUnitTest.XCode/Membership/AreaTests.cs
+++ XUnitTest.XCode/Membership/AreaTests.cs
@@ -259,6 +259,7 @@ public class AreaTests
             }
 
             //Area.Meta.Session.Truncate();
+            Area.Meta.Session.Truncate();
             var rs = Area.Import(file, true, 3, false);
             Assert.Equal(3624, rs);
 
