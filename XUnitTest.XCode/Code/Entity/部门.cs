4dd8dabdd704f947f67fa7c33c2eaadaae4571a0
猿人易 <qcjxberin@outlook.com>
2025-09-02 20:00:23 +0800

    [feat] 支持在数据类中生成Search高级查询方法。

--- XUnitTest.XCode/Code/Entity/部门.cs
+++ XUnitTest.XCode/Code/Entity/部门.cs
@@ -1,4 +1,4 @@
-using System;
+﻿using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Runtime.Serialization;
@@ -444,6 +444,38 @@ public partial class Department : IDepartment, IEntity<IDepartment>
         if (Meta.Session.Count < 1000) return Meta.Cache.FindAll(e => e.TenantId == tenantId);
 
         return FindAll(_.TenantId == tenantId);
+    }
+    #endregion
+
+    #region 高级查询
+    /// <summary>高级查询</summary>
+    /// <param name="tenantId">租户</param>
+    /// <param name="code">代码</param>
+    /// <param name="name">名称</param>
+    /// <param name="parentId">父级</param>
+    /// <param name="visible">可见</param>
+    /// <param name="managerId">管理者</param>
+    /// <param name="enable">启用</param>
+    /// <param name="start">更新时间开始</param>
+    /// <param name="end">更新时间结束</param>
+    /// <param name="key">关键字</param>
+    /// <param name="page">分页参数信息。可携带统计和数据权限扩展查询等信息</param>
+    /// <returns>实体列表</returns>
+    public static IList<Department> Search(Int32 tenantId, String? code, String name, Int32 parentId, Boolean? visible, Int32 managerId, Boolean? enable, DateTime start, DateTime end, String key, PageParameter page)
+    {
+        var exp = new WhereExpression();
+
+        if (tenantId >= 0) exp &= _.TenantId == tenantId;
+        if (!code.IsNullOrEmpty()) exp &= _.Code == code;
+        if (!name.IsNullOrEmpty()) exp &= _.Name == name;
+        if (parentId >= 0) exp &= _.ParentID == parentId;
+        if (visible != null) exp &= _.Visible == visible;
+        if (managerId >= 0) exp &= _.ManagerId == managerId;
+        if (enable != null) exp &= _.Enable == enable;
+        exp &= _.UpdateTime.Between(start, end);
+        if (!key.IsNullOrEmpty()) exp &= SearchWhereByKeys(key);
+
+        return FindAll(exp, page);
     }
     #endregion
 
