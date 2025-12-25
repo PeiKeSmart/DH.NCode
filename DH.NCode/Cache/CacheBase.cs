f0e73aeb5c72be198cd14b171d785995a18eb2c1
猿人易 <qcjxberin@outlook.com>
2025-12-25 17:02:06 +0800

    引入惰性消费者，让缓存更新的查询在异步任务中串行执行，避免并行查询卡死SQLite数据库。StarServer更新后，完成任务的曲线不再有明显凸起的波峰。

--- DH.NCode/Cache/CacheBase.cs
+++ DH.NCode/Cache/CacheBase.cs
@@ -1,7 +1,9 @@
 ﻿using System.Collections.Concurrent;
+
 using NewLife;
 using NewLife.Log;
 using NewLife.Threading;
+
 using XCode.DataAccessLayer;
 
 namespace XCode.Cache;
@@ -15,6 +17,9 @@ public abstract class CacheBase<TEntity> : CacheBase where TEntity : Entity<TEnt
 
     /// <summary>表名</summary>
     public String TableName { get; set; } = null!;
+
+    /// <summary>当前实体类专用的惰性消费者。让缓存更新异步串行执行，避免并行阻塞数据库</summary>
+    protected LazyConsumer Consumer { get; } = new();
     #endregion
 
     /// <summary>调用委托方法前设置连接名和表名，调用后还原</summary>
