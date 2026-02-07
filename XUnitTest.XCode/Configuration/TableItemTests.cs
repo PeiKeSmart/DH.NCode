b5a9082d6463198047cee414d77c78aba0f40b7d
猿人易 <qcjxberin@outlook.com>
2026-02-07 12:19:23 +0800

    TableItem属性变更自动同步DataTable，简化用法
    实现TableItem.ConnName/TableName属性变更时自动同步到DataTable，确保反向工程和元数据一致，无需手动同步。TableItem.Create方法签名简化，相关EntityFactory、EntitySession等调用同步调整。文档与单元测试已更新，提升易用性和一致性。

--- XUnitTest.XCode/Configuration/TableItemTests.cs
+++ XUnitTest.XCode/Configuration/TableItemTests.cs
@@ -1,6 +1,9 @@
 ﻿using NewLife.Log;
+
 using XCode.Configuration;
+
 using Xunit;
+
 using XUnitTest.XCode.TestEntity;
 
 namespace XUnitTest.XCode.Configuration;
@@ -10,7 +13,7 @@ public class TableItemTests
     [Fact]
     public void TrimIndex()
     {
-        var ti = TableItem.Create(typeof(Log2), null);
+        var ti = TableItem.Create(typeof(Log2));
         XTrace.WriteLine(ti.TableName);
         Assert.Equal(4, ti.DataTable.Indexes.Count);
     }
